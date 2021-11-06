cc923e192952ec23cfcfa3773470c3f63060cdb6
Andrey Martyn <martynandrey@rambler.ru>
2021-11-06 15:05:54 +0600

    Application is implemented in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
+        double accountBalance = 322.10;
+        double refillAmount = 1230;
+        int oneBonusCost = 100;
 
+        if (refillAmount < 0) {
+            System.out.println("The refill amount cannot be less than zero");
+        } else {
+            int bonus = 0;
+
+            if (refillAmount > 1000) {
+                if (oneBonusCost <= 0) {
+                    System.out.println("The cost of one bonus cannot be less than zero");
+                } else {
+                    bonus = (int)(refillAmount / oneBonusCost);
+                }
+            }
+
+            double total = accountBalance + refillAmount + bonus;
+            System.out.println("Bonus = " + bonus);
+            System.out.println("Account total = " + total);
+        }
     }
 }
